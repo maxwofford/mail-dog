817fe35fb97a02e3fbb8183a56bd5a87ee22ca28
Max Wofford <max@maxwofford.com>
2020-08-30 13:58:38 -0400

    Send message on test

--- features/interaction_test.js
+++ features/interaction_test.js
@@ -4,6 +4,6 @@ async function react(bot, addOrRemove, channel, timestamp, reaction) {
 module.exports = function(controller) {
   controller.hears(['test'], 'mention,direct_mention,direct_message', async(bot, message) => {
     react(bot, 'add', message.channel, message.ts, 'beachball')
-    
+    bot.reply(message, transcript('barkBark!'))
   })
 }
