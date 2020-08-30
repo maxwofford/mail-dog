3743dc11fbba019e1f7a2970621413957d485fb0
Max Wofford <max@maxwofford.com>
2020-08-30 13:44:51 -0400

    Attempt to fix channel object in test interaction

--- features/interaction_test.js
+++ features/interaction_test.js
@@ -3,6 +3,7 @@ async function react(bot, addOrRemove, channel, timestamp, reaction) {
 }
 module.exports = function(controller) {
   controller.hears(['test'], 'mention,direct_mention,direct_message', async(bot, message) => {
-    react(bot, 'add', message.channel.id, message.ts, 'beachball')
+    react(bot, 'add', message.channel, message.ts, 'beachball')
+    
   })
 }
