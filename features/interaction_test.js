6acfc8d08674dac6a7f971bb23e3d2260a8eb65b
Max Wofford <max@maxwofford.com>
2020-08-30 13:41:48 -0400

    Add reaction test interaction

---
+++ features/interaction_test.js
@@ -0,0 +1,8 @@
+async function react(bot, addOrRemove, channel, timestamp, reaction) {
+  return await bot.api.reactions[addOrRemove]({timestamp, channel, name: reaction})
+}
+module.exports = function(controller) {
+  controller.hears(['test'], 'mention,direct_mention,direct_message', async(bot, message) => {
+    react(bot, 'add', message.channel.id, message.ts, 'beachball')
+  })
+}
