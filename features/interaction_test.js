41f1d5a3c611c53458a141e929ae3184eed96f18
Max Wofford <max@maxwofford.com>
2020-08-30 14:01:51 -0400

    test runs for regular messages

--- features/interaction_test.js
+++ features/interaction_test.js
@@ -2,7 +2,7 @@ async function react(bot, addOrRemove, channel, timestamp, reaction) {
   return await bot.api.reactions[addOrRemove]({timestamp, channel, name: reaction})
 }
 module.exports = function(controller) {
-  controller.hears(['test'], 'mention,direct_mention,direct_message', async(bot, message) => {
+  controller.hears(['test', 'send'], 'mention,message,direct_mention,direct_message', async(bot, message) => {
     react(bot, 'add', message.channel, message.ts, 'beachball')
     bot.reply(message, transcript('barkBark!'))
   })
