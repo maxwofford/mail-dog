3c2587bc18c56a1ac257f9f2b1684ebb96831ebe
Max Wofford <max@maxwofford.com>
2020-08-31 00:13:40 -0400

    Add experimental animated speak command

--- features/interaction_speak.js
+++ features/interaction_speak.js
@@ -1,7 +1,54 @@
 const transcript = require('../utils/transcript')
+
 module.exports = function(controller) {
-  controller.hears(['speak', 'say', 'talk'], 'mention,message,direct_message,direct_mention', async(bot, message) => {
+  async function replyWithFlavor(message, text) {
+    const bot = await controller.spawn({token: process.env.BOT_TOKEN})
+    // const wordsArr = text.split(' ')
+    // console.log(wordsArr)
+
+    // const sent = await bot.reply(message, wordsArr[0])
+    // await bot.updateMessage({text, ...sent})
+    let sent = await bot.reply(message,'this is my original reply...');
+
+// update the sent message using the sent.id field
+await bot.updateMessage({
+     text: 'this is an update!',
+     ...sent
+})
+  }
+  controller.hears(['speak', 'say', 'talk'], ['mention','message','direct_message','direct_mention'], async(bot, message) => {
     console.log('I heard master say something!')
-    await bot.reply(message, transcript('barkBark!'))
+    // await bot.reply(message, transcript('barkBark!'))
+
+    const text = transcript('barkBark!')
+
+    const wordsArray = text.split(' ')
+    let sent
+    for (let i=0; i<=wordsArray.length; i++) {
+      let word = wordsArray[i]
+      let previousWords = wordsArray.slice(0,i).join(' ')
+      let currentText = previousWords
+      if (word) {
+        currentText+= ' _*' + word.toUpperCase() + '*_'
+      }
+      if (!sent) {
+        sent = await bot.reply(message, currentText);
+      } else if (word) {
+        sent = {...(await bot.updateMessage({ text: currentText, ...sent })), ...sent}
+      } else {
+        sent = {...(await bot.updateMessage({ text: currentText, ...sent })), ...sent}
+      }
+    }
+
+    await bot.updateMessage({
+      text,
+      ...sent
+    })
+
+    // update the sent message using the sent.id field
+    await bot.updateMessage({
+      text,
+      ...sent
+    })
   })
 }
