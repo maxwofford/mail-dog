3dcf3b6ba4574923fd54f426d04e730af6be6087
Max Wofford <max@maxwofford.com>
2020-09-08 23:20:07 -0400

    Try/Catch Drop & Test commands

--- features/interaction_drop.js
+++ features/interaction_drop.js
@@ -17,71 +17,75 @@ module.exports = function(controller) {
       return
     }
 
-    const results = {}
-    await Promise.all([
-      react('add', message.channel, message.ts, 'beachball'),
-      airFind('Mail Missions', 'Mail Team Thread Timestamp', message.thread_ts).then(mission => (
-        results.mission = mission
-      )),
-      airFind('Mail Senders', 'Slack ID', message.user).then(
-        sender => (results.sender = sender)
-      ),
-    ])
+    try {
+      const results = {}
+      await Promise.all([
+        react('add', message.channel, message.ts, 'beachball'),
+        airFind('Mail Missions', 'Mail Team Thread Timestamp', message.thread_ts).then(mission => (
+          results.mission = mission
+        )),
+        airFind('Mail Senders', 'Slack ID', message.user).then(
+          sender => (results.sender = sender)
+        ),
+      ])
 
-    if (!results.sender) {
-      await Promise.all([
-        bot.replyInThread(message, transcript('drop.noSender')),
-        react('remove', message.channel, message.ts, 'beachball'),
-        react('add', message.channel, message.ts, 'warning'),
-      ])
-      return
-    }
+      if (!results.sender) {
+        await Promise.all([
+          bot.replyInThread(message, transcript('drop.noSender')),
+          react('remove', message.channel, message.ts, 'beachball'),
+          react('add', message.channel, message.ts, 'warning'),
+        ])
+        return
+      }
 
-    if (!results.mission) {
-      await Promise.all([
-        bot.replyInThread(message, transcript('drop.missionNotFound')),
-        react('remove', message.channel, message.ts, 'beachball'),
-        react('add', message.channel, message.ts, 'warning'),
-      ])
-      return
-    }
+      if (!results.mission) {
+        await Promise.all([
+          bot.replyInThread(message, transcript('drop.missionNotFound')),
+          react('remove', message.channel, message.ts, 'beachball'),
+          react('add', message.channel, message.ts, 'warning'),
+        ])
+        return
+      }
 
-    if (results.sender.fields['Permissions'].indexOf('Drop') == -1) {
-      await Promise.all([
-        bot.replyInThread(message, transcript('drop.missingPermission')),
-        react('remove', message.channel, message.ts, 'beachball'),
-        react('add', message.channel, message.ts, 'warning'),
-      ])
-      return
-    }
+      if (results.sender.fields['Permissions'].indexOf('Drop') == -1) {
+        await Promise.all([
+          bot.replyInThread(message, transcript('drop.missingPermission')),
+          react('remove', message.channel, message.ts, 'beachball'),
+          react('add', message.channel, message.ts, 'warning'),
+        ])
+        return
+      }
 
-    if (results.mission.fields['Dropped']) {
-      await Promise.all([
-        bot.replyInThread(message, transcript('drop.alreadyDropped')),
-        react('remove', message.channel, message.ts, 'beachball'),
-        react('add', message.channel, message.ts, 'white_check_mark'),
-      ])
-    } else {
-      await Promise.all([
-        bot.replyInThread(message, {
-          blocks: [
-            {
-              "type": "image",
-              "title": {
-                "type": "plain_text",
-                "text": "Dropped!",
-                "emoji": true
-              },
-              "image_url": transcript('drop.success'),
-              "alt_text": "mission dropped"
-            }
-          ]
-        }),
-        airPatch('Mail Missions', results.mission.id, { 'Dropped': true }),
-        react('remove', message.channel, message.ts, 'beachball'),
-        react('add', message.channel, message.ts, 'white_check_mark'),
-        react('add', message.channel, results.mission.fields['Mail Team Thread Timestamp'], 'skull')
-      ])
+      if (results.mission.fields['Dropped']) {
+        await Promise.all([
+          bot.replyInThread(message, transcript('drop.alreadyDropped')),
+          react('remove', message.channel, message.ts, 'beachball'),
+          react('add', message.channel, message.ts, 'white_check_mark'),
+        ])
+      } else {
+        await Promise.all([
+          bot.replyInThread(message, {
+            blocks: [
+              {
+                "type": "image",
+                "title": {
+                  "type": "plain_text",
+                  "text": "Dropped!",
+                  "emoji": true
+                },
+                "image_url": transcript('drop.success'),
+                "alt_text": "mission dropped"
+              }
+            ]
+          }),
+          airPatch('Mail Missions', results.mission.id, { 'Dropped': true }),
+          react('remove', message.channel, message.ts, 'beachball'),
+          react('add', message.channel, message.ts, 'white_check_mark'),
+          react('add', message.channel, results.mission.fields['Mail Team Thread Timestamp'], 'skull')
+        ])
+      }
+    } catch (err) {
+      bot.replyInThread(message, transcript('errors.generalFormat', {err}))
     }
   })
 }
--- features/interaction_test.js
+++ features/interaction_test.js
@@ -13,93 +13,96 @@ module.exports = function(controller) {
 
   // @maildog test sticker_envelope @orpheus reward for being such a good dino
   controller.hears(['test', 'send'], ['mention','direct_message','direct_mention'], async(bot, message) => {
-    console.log(message.text)
-    const cleanText = message.text.replace(/ +/g, " ").trim()
-    const verb = cleanText.split(' ')[0]
-    const scenarioName = cleanText.split(' ')[1]
-    const recipientID = cleanText.split(' ')[2].match(/[A-Z0-9]+/g)[0]
-    const note = cleanText.split(' ').slice(3).join(' ')
-    console.log(verb, scenarioName, recipientID, note)
+    try {
+      const cleanText = message.text.replace(/ +/g, " ").trim()
+      const verb = cleanText.split(' ')[0]
+      const scenarioName = cleanText.split(' ')[1]
+      const recipientID = cleanText.split(' ')[2].match(/[A-Z0-9]+/g)[0]
+      const note = cleanText.split(' ').slice(3).join(' ')
+      console.log(verb, scenarioName, recipientID, note)
 
-    const results = {}
-    await Promise.all([
-      react('add', message.channel, message.ts, 'beachball'),
-      airFind('Mail Senders', 'Slack ID', message.user).then(
-        sender => (results.sender = sender)
-      ),
-      airFind('Mail Scenarios', 'ID', scenarioName).then(
-        scenario => (results.scenario = scenario)
-      ),
-      airFind('People', 'Slack ID', recipientID).then(async recipient => {
-        if (recipient) {
-          return recipient
-        } else {
-          const ORPHEUS_CHANNEL = 'GP36BNYV8'
-          const ORPHEUS_ID = 'UM1L1C38X'
-          console.log("Asking Orpheus to find or create this person because I can't find them in the db")
-          await Promise.all([
-            await bot.say({channel: ORPHEUS_CHANNEL, text: `<@${ORPHEUS_ID}> find or create <@${recipientID}>` }),
-            new Promise(resolve => setTimeout(resolve, 5000))
-          ])
-          return await airFind('People', 'Slack ID', recipientID)
-        }
-      }).then(
-        recipient => (results.recipient = recipient)
-      ),
-    ])
-
-    if (!results.sender) {
-      bot.replyInThread(message, transcript('test.noSender'))
-      return
-    }
-    if (results.sender.fields['Permissions'].indexOf('Test') == -1) {
-      bot.replyInThread(message, transcript('test.missingPermission'))
-      return
-    }
-    if (!results.scenario) {
+      const results = {}
       await Promise.all([
-        bot.replyInThread(message, transcript('test.invalidScenario', {scenarioName})).then(
-          reply => results.reply = reply
+        react('add', message.channel, message.ts, 'beachball'),
+        airFind('Mail Senders', 'Slack ID', message.user).then(
+          sender => (results.sender = sender)
         ),
-        airGet('Mail Scenarios').then(
-          scenarios => (
-            results.scenarios = scenarios.map(s => s.fields['ID'])
-          )
+        airFind('Mail Scenarios', 'ID', scenarioName).then(
+          scenario => (results.scenario = scenario)
         ),
-        react('remove', message.channel, message.ts, 'beachball'),
-        react('add', message.channel, message.ts, 'warning'),
+        airFind('People', 'Slack ID', recipientID).then(async recipient => {
+          if (recipient) {
+            return recipient
+          } else {
+            const ORPHEUS_CHANNEL = 'GP36BNYV8'
+            const ORPHEUS_ID = 'UM1L1C38X'
+            console.log("Asking Orpheus to find or create this person because I can't find them in the db")
+            await Promise.all([
+              await bot.say({channel: ORPHEUS_CHANNEL, text: `<@${ORPHEUS_ID}> find or create <@${recipientID}>` }),
+              new Promise(resolve => setTimeout(resolve, 5000))
+            ])
+            return await airFind('People', 'Slack ID', recipientID)
+          }
+        }).then(
+          recipient => (results.recipient = recipient)
+        ),
       ])
-      const Fuse = require('fuse.js')
-      const fuse = new Fuse(results.scenarios)
-      const sorted = fuse.search(scenarioName).map(s => s.item)
 
-      if (sorted.length > 0) {
-        await bot.replyInThread(message, transcript('test.recommendedScenarios', {sorted}))
+      if (!results.sender) {
+        bot.replyInThread(message, transcript('test.noSender'))
+        return
       }
-      return
-    }
+      if (results.sender.fields['Permissions'].indexOf('Test') == -1) {
+        bot.replyInThread(message, transcript('test.missingPermission'))
+        return
+      }
+      if (!results.scenario) {
+        await Promise.all([
+          bot.replyInThread(message, transcript('test.invalidScenario', {scenarioName})).then(
+            reply => results.reply = reply
+          ),
+          airGet('Mail Scenarios').then(
+            scenarios => (
+              results.scenarios = scenarios.map(s => s.fields['ID'])
+            )
+          ),
+          react('remove', message.channel, message.ts, 'beachball'),
+          react('add', message.channel, message.ts, 'warning'),
+        ])
+        const Fuse = require('fuse.js')
+        const fuse = new Fuse(results.scenarios)
+        const sorted = fuse.search(scenarioName).map(s => s.item)
 
-    const fetch = require('node-fetch')
-    const response = await fetch('https://hooks.zapier.com/hooks/catch/507705/o2dbufn/', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({
-        test: verb == 'test',
-        scenarioRecordID: results.scenario.id,
-        receiverAddressRecordID: results.recipient.fields['Address'][0],
-        missionNotes: note + ' mission created through maildog'
+        if (sorted.length > 0) {
+          await bot.replyInThread(message, transcript('test.recommendedScenarios', {sorted}))
+        }
+        return
+      }
+
+      const fetch = require('node-fetch')
+      const response = await fetch('https://hooks.zapier.com/hooks/catch/507705/o2dbufn/', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          test: verb == 'test',
+          scenarioRecordID: results.scenario.id,
+          receiverAddressRecordID: results.recipient.fields['Address'][0],
+          missionNotes: note + ' mission created through maildog'
+        })
       })
-    })
-    if (response.statusText == 'OK') {
-      await Promise.all([
-        react('remove', message.channel, message.ts, 'beachball'),
-        react('add', message.channel, message.ts, 'white_check_mark'),
-      ])
-    } else {
-      await Promise.all([
-        react('remove', message.channel, message.ts, 'beachball'),
-        react('add', message.channel, message.ts, 'warning'),
-      ])
+      if (response.statusText == 'OK') {
+        await Promise.all([
+          react('remove', message.channel, message.ts, 'beachball'),
+          react('add', message.channel, message.ts, 'white_check_mark'),
+        ])
+      } else {
+        await Promise.all([
+          react('remove', message.channel, message.ts, 'beachball'),
+          react('add', message.channel, message.ts, 'warning'),
+        ])
+      }
+    } catch (err) {
+      bot.replyInThread(message, transcript('errors.generalFormat', {err}))
     }
   })
 }
