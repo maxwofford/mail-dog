038cd309d3c8243618e9ac6e5761d04a636b00b8
Max Wofford <max@maxwofford.com>
2020-10-17 14:41:23 -0400

    Indent logs

--- features/interaction_accept.js
+++ features/interaction_accept.js
@@ -12,11 +12,14 @@ module.exports = function(controller) {
   }
 
   controller.hears(/^accept/, ['direct_mention','bot_message'], async(bot, message) => {
-    console.log(message)
+    console.log(JSON.stringify(message, null, 2))
     if (message.channel != 'GNTFDNEF8' || !message.thread_ts) {
       // just ignore it
       return
     }
+    // if (message.type == 'bot_message') {
+    //   message.user = message.bot.id
+    // }
 
     const results = {}
 
